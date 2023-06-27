720713bd8a7d35db996ff4764778a3b7c511efce
osykris <osy.krisdayanti27@gmail.com>
2023-06-27 12:44:51 +0700

    final

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>Privacy and Policy</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="css/main.css">
+
+</head>
+
+<body>
+
+    <!-- MENU BAR -->
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand" href="index.html">
+                <img src="images/logo.png" width="120px" />
+                Medisnet App
+            </a>
+        </div>
+    </nav>
+
+    <section class="hero hero-bg d-flex justify-content-center align-items-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-10 col-12 d-flex flex-column justify-content-center align-items-center">
+                    <div class="hero-text">
+                        <h1 class="text-white" data-aos="fade-up">Privacy and Policy - Medisnet App</h1>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-12">
+                    <div class="hero-image" data-aos="fade-up" data-aos-delay="300">
+                        <img src="images/working-girl.png" class="img-fluid" alt="working girl">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- ABOUT -->
+    <section class="about section-padding pb-0" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 mx-auto col-md-10 col-12">
+                    <div class="about-info">
+                        <p class="mb-0" style="text-align: justify;" data-aos="fade-up">Kebijakan Privasi ini menjelaskan bagaimana MHDC Group
+                            melindungi dan mengelola Data Pribadi Anda saat mengakses dan menggunakan Fitur yang ada
+                            pada Medis App. Data Pengguna yang sudah diperoleh dari MHDC Group mencakup informasi yang
+                            dapat mengidentifikasi Anda secara langsung.
+                            <br><br>Fitur yang ada pada Aplikasi Medis App dirancang untuk mengakomodir dan memenuhi
+                            kebutuhan Pengguna selaku Dokter yang ada di MHDC Group dalam rangka meningkatkan
+                            produktivitas, efektifitas dan efsiensi waktu dalam memberikan pelayanan yang lebih baik
+                            kepada pasien.
+                            <br><br>
+                            Aplikasi medis app MHDC Group menjaga privasi Anda dengan serius menggunakan teknologi
+                            keamanan yang dikelola oleh MHDC Group dengan kebijakan dan persyaratan yang sudah
+                            disepakati antara Pengguna dan MHDC Group.
+                            <br><br>
+                            Medis App tidak membagikan informasi pribadi Anda dengan pihak ketiga. kecuali sebagaimana
+                            diharuskan oleh hukum atau peraturan yang berlaku.
+                        </p>
+                    </div>
+
+                    <div class="about-image" data-aos="fade-up" data-aos-delay="200">
+                        <img src="images/office.png" class="img-fluid" alt="office">
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+
+    <footer class="site-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 mx-lg-auto col-md-8 col-10">
+                    <h1 class="text-white" data-aos="fade-up" data-aos-delay="100"> <img src="images/logo.png"
+                            width="120px" /> <strong>Medisnet</strong> App.</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 mx-lg-auto text-center col-md-8 col-12" data-aos="fade-up" data-aos-delay="400">
+                    <p class="copyright-text">Copyright &copy; 2023 Medisnet App</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+    <!-- SCRIPTS -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/aos.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/smoothscroll.js"></script>
+    <script src="js/custom.js"></script>
+
+</body>
+
+</html>
